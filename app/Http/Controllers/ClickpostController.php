7ac67d0f261a8bf3a31234baffaed736bbb1b340
Mahendra Vala <mahendravala1986@gmail.com>
2022-10-29 11:28:49 +0530

    Update ClickpostController.php

--- app/Http/Controllers/ClickpostController.php
+++ app/Http/Controllers/ClickpostController.php
@@ -268,7 +268,7 @@ class ClickpostController extends Controller
             'query' => [
                 //'username' => 'printstop-test'
                 //'key'=>'5f0e9f97-6c01-4d70-acc3-d23aa426c682'
-                'key'=>'58b63829-3df4-453f-9724-00603323ffb3'
+                'key'=>'58b63829-3df4-453f-9724-00603323ffb3',
                 'username' => 'printstop'
             ]
         ]);
